063c035a54a3e49f70bcd2619cb2c964ce5e1a8f
Eslam Mohamed <emohamef733@gmail.com>
2023-08-12 19:50:00 +0300

    change route type

--- routes/api.php
+++ routes/api.php
@@ -62,7 +62,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::group(['name' => 'Users', 'prefix' => 'users'], function () {
         Route::get('/', [UserController::class, 'index']);
-        Route::get('/delete/{id}', [UserController::class, 'destory']);
+        Route::delete('/delete/{id}', [UserController::class, 'destory']);
     });
     
 });

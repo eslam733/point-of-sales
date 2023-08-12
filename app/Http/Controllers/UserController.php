eaaa410db3b6a326ceb4c97f34f523ded7e85397
Eslam Mohamed <emohamef733@gmail.com>
2023-08-12 19:52:16 +0300

    validation if user not found in delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,10 @@ class UserController extends Controller
     public function destory(Request $request, $id) {
         $user = User::where('id', $id)->first();
 
+        if (empty($user)) return $this->errorResponse('user not found', [], 404);
+
         if ($user->isAdmin()) {
-            return $this->successResponse('can\'t delete admin', [], 403);
+            return $this->errorResponse('can\'t delete admin', [], 403);
         }
 
         $user->delete();
